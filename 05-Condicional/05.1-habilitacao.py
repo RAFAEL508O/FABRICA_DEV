75ca81f149aee2e3daa328635f9b8fc52f2ced05
Raphael Pinheiro Baia <raphaelpinheirobaia6@gmail.com>
2026-05-30 11:53:51 -0300

    Add files via upload

---
+++ 05-Condicional/05.1-habilitacao.py
@@ -0,0 +1,9 @@
+#Criando as variáveis
+nome = input("qual é o seu nome?")
+idade = int(input("qual é a sua idade?"))  
+
+#verificando condição da idade
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
