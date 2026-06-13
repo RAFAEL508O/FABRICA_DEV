6aada331376b8bc13ddb52aa8f4ffe7be43dca1d
Raphael Pinheiro Baia <raphaelpinheirobaia6@gmail.com>
2026-06-13 11:44:19 -0300

    Delete 05.1-maiordeidade.py

--- 05.1-maiordeidade.py
+++
@@ -1,9 +0,0 @@
-#Criando as variáveis
-nome = input("qual é o seu nome?")
-idade = int(input("qual é a sua idade?"))  
-
-#verificando condição da idade
-if idade >= 18:
-    print("Maior de idade")
-else:
-    print("Menor de idade")
